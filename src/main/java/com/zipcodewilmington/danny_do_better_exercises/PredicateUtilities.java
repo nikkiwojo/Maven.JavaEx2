9f96b80cae7bafc7a4924a0f1fcd25e6f84ea49f
nikkiwojo <nwojewodzki@gmail.com>
2021-08-29 14:07:48 -0400

    Predicate Done

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,13 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        Boolean answer;
+        if (x > y) {
+           answer = true; 
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     /**
@@ -19,7 +25,13 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        Boolean answer;
+        if (x < y) {
+           answer = true; 
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     /**
@@ -28,7 +40,13 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        Boolean answer;
+        if (x >= y) {
+           answer = true; 
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     /**
@@ -37,7 +55,13 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        Boolean answer;
+        if (x <= y) {
+           answer = true; 
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
 
@@ -45,14 +69,14 @@ public class PredicateUtilities {
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
